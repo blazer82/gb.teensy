112e4cc725f19f58cca5f9809e621d117c674219
Raphael Stäbler <raphael@productionbuild.de>
2020-01-19 18:43:54 +0100

    Updated license headers

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
  * This file is part of the gb.teensy emulator
  * <https://github.com/blazer82/gb.teensy>.
  * 
- * It is free software: you can redistribute it and/or modify
+ * This is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * It is distributed in the hope that it will be useful,
+ * This is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
