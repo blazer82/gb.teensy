64727155b03373c200ac391387601ec19a7d36a9
Raphael Stäbler <raphael@productionbuild.de>
2020-06-13 16:50:40 +0200

    Add macor for multiple cartridges

--- lib/Cartridge/Cartridge.h
+++ lib/Cartridge/Cartridge.h
@@ -2,7 +2,15 @@
 
 #include <sys/_stdint.h>
 
+#define GAME_TETRIS 1
+// #define GAME_SUPER_MARIO 1
+
 class Cartridge {
    public:
+#ifdef GAME_TETRIS
     static const uint8_t cartridge[0x8000];
+#endif
+#ifdef GAME_SUPER_MARIO
+    static const uint8_t cartridge[0x10000];
+#endif
 };
