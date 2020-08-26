4a7dbe5e5ad0bbd114da5c4e40a81f1fe763864b
granthaack <granthaack@protonmail.com>
2020-08-25 22:10:23 -0700

    started work on MBC2

---
+++ lib/Cartridge/MBC2.cpp
@@ -0,0 +1,69 @@
+#include "MBC2.h"
+
+MBC2::MBC2(const char *romFile) : Cartridge(romFile){
+    // Initialize the control registers
+    ramEnable = 0x0;
+    romBankSelect = 0x1; // Defaults to bank 1 on PoR
+
+    // Allocate memory for ROM banks
+    romBanks = (uint8_t**)malloc(romBankCount * sizeof(uint8_t *));
+    for (uint8_t i = 0; i < romBankCount; i++){
+        romBanks[i] = (uint8_t*)malloc(ROM_BANK_SIZE * sizeof(uint8_t));
+    }
+
+    // Allocate memory for the only RAM bank
+    // Technically this could be cut in half since the MBC2 only uses 4
+    // bits of ROM per address, but that would probably slow things down
+    // and we have plenty of RAM.
+    ramBank = (uint8_t*)malloc(MBC2_CART_RAM_TOP - CART_RAM * sizeof(uint8_t))
+}
+
+MBC2::~MBC2(){
+    Serial.println("Deleting MBC2");
+}
+
+uint8_t MBC2::readByte(uint16_t addr){
+    // Handle reads from RAM
+    if(addr >= CART_RAM && addr <= MBC2_CART_RAM_TOP){
+        
+    }
+    // Handle reads from banked cartridge ROM
+    else if(addr >= CART_ROM_BANKED){
+
+    }
+    // Handle reads from ROM bank zero
+    else if(addr >= CART_ROM_ZERO){
+
+    }
+    // MISRA
+    else{
+        // Assume invalid reads return 0xFF
+        // TODO: Look this up
+        Serial.printf("ERROR: Attempted to read from invalid address in MBC2 cartridge: 0x%04x\n", addr);
+        return 0xFF;
+    } 
+}
+
+void MBC2::writeByte(uint16_t addr, uint8_t data){
+    // Handle writes to RAM
+    if(addr >= CART_RAM && addr <= MBC2_CART_RAM_TOP){
+        // Make sure RAM is enabled and it exists
+        if(ramEnable && ramBankCount > 0){
+        }
+    }
+    // Handle writes to control registers
+    // This write function ensures that all data written to control registers
+    // is valid. Additional checking elsewhere is not needed
+    // Manipulate the bank select register
+    else if(addr >= MBC2_PRIMARY_BANK_REG && addr <= MBC2_PRIMARY_BANK_REG_TOP){
+
+    }
+    // Manipulate the RAM enable register
+    else if(addr >= MBC2_RAM_ENABLE_REG){
+        
+    }
+    // MISRA
+    else{
+        Serial.printf("ERROR: Attempted to write 0x%04x to invalid address 0x%04x in MBC2 cartridge\n", data, addr);
+    } 
+}
---
+++ lib/Cartridge/MBC2.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <sys/_stdint.h>
+#include "Cartridge.h"
+#include <stdlib.h>
+#include <Arduino.h>
+
+// Control Register Addresses
+#define MBC2_RAM_ENABLE_REG          0x0000
+#define MBC2_PRIMARY_BANK_REG        0x2000
+
+// MBC2 Has significantly less RAM than can fit in the memory map
+#define MBC2_CART_RAM_TOP            0xA200
+
+class MBC2 : public Cartridge{
+    public:
+        MBC2(const char *romFile);
+        ~MBC2();
+        uint8_t readByte(uint16_t addr) override;
+        void writeByte(uint16_t addr, uint8_t data) override;
+    private:
+        // Enable/Disable the RAM
+        uint8_t ramEnable;
+        // Select the ROM bank, 0x0 - 0x0F
+        uint8_t romBankSelect;
+        // The amount of ROM banks in the cart
+        uint8_t romBankCount;
+
+        // TODO: Allocate these in PSRAM
+        // ROM banks
+        uint8_t** romBanks;
+        // The RAM bank
+        uint8_t* ramBank;
+};
