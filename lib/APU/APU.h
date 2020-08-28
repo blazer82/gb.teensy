1618aa0366e08ec54203a55757cf2484b256cb21
Raphael Stäbler <raphael@productionbuild.de>
2020-08-28 16:05:58 +0200

    Add APU with square wave POC

---
+++ lib/APU/APU.h
@@ -0,0 +1,35 @@
+/**
+ * gb.teensy Emulation Software
+ * Copyright (C) 2020  Raphael Stäbler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
+
+#pragma once
+
+#include <Arduino.h>
+
+#define AUDIO_OUT 7
+
+class APU {
+   public:
+    static void begin();
+    static void apuStep();
+
+   protected:
+    static IntervalTimer apuTimer;
+    static volatile bool state;
+
+   private:
+};
